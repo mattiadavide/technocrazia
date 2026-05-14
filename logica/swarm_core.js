49108941bea1c524739eaada5a9444c5f485baa9
mattiadavide <mattiadavide@users.noreply.github.com>
2026-05-14 20:31:10 +0000

    deploy: mattiadavide/technocrazia_root@0544c28c05736fbe5e669bbd6e4fcc3f2ab4d047

--- logica/swarm_core.js
+++ logica/swarm_core.js
@@ -1,16 +1,1 @@
-export const PHI = 1.6180339887;
-export const SWARM_SIZE = 60000;
-export const STRIDE = 12; // AUMENTATO DA 11 A 12
-
-export const IDX_X = 0;
-export const IDX_Y = 1;
-export const IDX_TX = 2;
-export const IDX_TY = 3;
-export const IDX_OPACITY = 4;
-export const IDX_ACTIVE = 5;
-export const IDX_PRIO = 6;
-export const IDX_REACT = 7;
-export const IDX_SCALE = 8;
-export const IDX_T_OPACITY = 9;
-export const IDX_SYNC = 10;
-export const IDX_Z = 11; // NUOVA VARIABILE PER IL Z-BUFFER
+export const PHI=1.6180339887;export const SWARM_SIZE=0xea60;export const STRIDE=0xc;export const IDX_X=0x0;export const IDX_Y=0x1;export const IDX_TX=0x2;export const IDX_TY=0x3;export const IDX_OPACITY=0x4;export const IDX_ACTIVE=0x5;export const IDX_PRIO=0x6;export const IDX_REACT=0x7;export const IDX_SCALE=0x8;export const IDX_T_OPACITY=0x9;export const IDX_SYNC=0xa;export const IDX_Z=0xb;
